ad9a619db08f97109f2193e75a7880fe0608d88e
avayedawadi <avaye.dawadi@gmail.com>
2020-04-29 15:38:55 -0400

    Adding comments and documentation to the code

--- content.js
+++ content.js
@@ -1,16 +1,23 @@
+/*This checks the URL to be selected good or bad which then sends that message to background.js
+which then actually notifies the user of their selection with a Chrome Rich Notification*/
 window.onload = function() {
     url = window.location.href;
     title = document.title;
     
+    /* The Chrome storage takes the URL that is being accessed to notify the user*/
     chrome.storage.sync.get(["master"], function(result) {
+        /*The correct master URL is put to a variable*/
         var res = result["master"];
-        // TODO: Parse the data and check to see if a judgement has been passed, or if there is a similar recipie
 
-        // First figure out if a judgmenet has been passed already.
+        // First figure out if a judgmenet has been passed already
+        //If it's not undefined then that means there is a master cookie for URL storage for use
         if (res != undefined) {
             var resArry = res.split("&&");
             resArry.forEach(element => {
+                //This checks that element contains the URL
                 if (element.includes(url)) {
+                    //This if/else checks if the URL has been previously selected as good
+                    //and then actually sends the message to background.js for use
                     if (element.includes("good")) {
                         // element good
                         chrome.extension.sendMessage({greeting: "good"}, function(response) {});
